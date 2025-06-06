01759eb76d9bfe98713709d1fc778c064411e94d
jasv11 <vadlamudijaswanth445@gmail.com>
2025-06-06 15:15:36 +0530

    Task two

---
+++ java/task002.java
@@ -0,0 +1,20 @@
+public class task002 {
+    
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+    
+        int result1 = add(5, 10);
+        System.out.println(result1);
+
+    
+        int result2 = add(20, 30);
+        System.out.println(result2);
+
+        
+        int result3 = add(100, 200);
+        System.out.println(result3);
+    }
+}
---
+++ java/task011.java
@@ -0,0 +1,39 @@
+public class task011 {
+    public static void main(String[] args) {
+        int day = 1;
+
+        System.out.println("===================");
+        System.out.println("Week Day Checker");
+        System.out.println("===================");
+        System.out.println("Input number: " + day);
+        System.out.print("Day is: ");
+
+        switch(day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid input");
+                System.out.println("Please enter a number between 1 and 7");
+        }
+        System.out.println("===================");
+    }
+}
